e382910442f05505cce98efbe3f87a1c94568c88
Ian Silva <ian.alves21@gmail.com>
2021-11-18 13:08:38 -0400

    Create player selection page and logic

--- Assets/Project/Online/Scripts/LobbyController.cs
+++ Assets/Project/Online/Scripts/LobbyController.cs
@@ -3,6 +3,7 @@ using Photon.Pun;
 using Photon.Pun.Demo.Cockpit.Forms;
 using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Project.Online.Scripts
 {
@@ -10,8 +11,11 @@ namespace Project.Online.Scripts
     {
         [SerializeField] private GameObject startButton;
         [SerializeField] private GameObject cancelButton;
+        [SerializeField] private GameObject[] gameObjectsToActive;
         [SerializeField] private GameObject[] availableCharactersPrefabs;
         [SerializeField] private int roomSize;
+
+        [SerializeField] private Button nextButton;
         
         private LobbyController lobby;
         
@@ -23,10 +27,14 @@ namespace Project.Online.Scripts
 
         public void StartButtonClick()
         {
-            startButton.SetActive(false);
-            cancelButton.SetActive(true);
-            PhotonNetwork.JoinRandomRoom();
-            Debug.Log("Start game");
+            // startButton.SetActive(false);
+            // cancelButton.SetActive(true);
+            // PhotonNetwork.JoinRandomRoom();
+            // Debug.Log("Start game");
+            foreach (var go in gameObjectsToActive)
+            {
+                go.SetActive(!go.activeSelf);
+            }
         }
         
         public void CancelButtonClick()
@@ -35,6 +43,35 @@ namespace Project.Online.Scripts
             cancelButton.SetActive(false);
             PhotonNetwork.LeaveRoom();
             Debug.Log("Cancel");
+        }
+        
+        public void CharacterButtonClick(int selectedCharacter)
+        {
+            switch(selectedCharacter)
+            {
+                case 0:
+                    RoomController.Room.selectedCharacter = "CyborgPlayer";
+                    break;
+                case 1:
+                    RoomController.Room.selectedCharacter = "PunkPlayer";
+                    break;
+                case 2:
+                    RoomController.Room.selectedCharacter = "BikerPlayer";
+                    break;
+                default:
+                    RoomController.Room.selectedCharacter = "BikerPlayer";
+                    break;
+            }
+
+            nextButton.interactable = true;
+        }
+        
+        public void NextButtonClick()
+        {
+            startButton.SetActive(false);
+            cancelButton.SetActive(true);
+            PhotonNetwork.JoinRandomRoom();
+            Debug.Log("Start game");
         }
         
         private void CreateRoom()
